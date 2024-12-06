46afa2b7ac077fc7640be18388010c53cbab9d02
Samanta Melissa <samanta.melissa13@gmail.com>
2024-12-06 10:54:20 -0300

    Subindo a aula um

---
+++ PrimeiraAula/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine -> Escreve uma linha no nosso terminal
+//Console.ReadLine -> LÊ uma linha no nosso terminal, INSERIDA PELO USUÁRIO.
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Olá, seja bem vindo ao meu novo sistema console!");
+Console.WriteLine("Informe seu nome:");
+string nome = Console.ReadLine();
+Console.WriteLine("Informe sua idade:");
+Console.ReadLine();
+
+Console.Write($"Seja bem vindo(a), {nome}! Fique a vontade!");
+// Console.WriteLine("Seja bem vindo(a)," + nome + "! Fique a vontade!");
+
+Console.WriteLine(@"Estamos felizes de ter você no nosso sistema. 
+Qualquer dúvida entre em contato com o suporte!");
+
+//Tipos de dados:
+//string
+//float
+//int 
+//bool
+string email = "samanta@email.com";
+float peso = 60.30f;
+int idade = 80;
+bool rica = false;
+
+
